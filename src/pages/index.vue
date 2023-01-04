d86239ee44c9caf2ad31671de895297925897ee4
motomichi <motomichi.motomichi@gmail.com>
2023-01-04 17:25:14 +0900

    v-selectの選択肢の見た目と選択済みアイテムの見た目を変更する

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,16 +3,56 @@
 
 <script setup lang="ts">
 const pageName = 'index-page';
+const state = reactive({
+  value: -1,
+});
+const onClickCustomItem = (value: number) => {
+  state.value = value;
+};
 </script>
 
 <template>
   <div class="index-page">
     <div>{{ pageName }}</div>
+
+    <client-only>
+      <v-select
+        v-model="state.value"
+        :items="[
+          { id: -1, name: '選択してください', value: -1 },
+          { id: 1, name: 'name1', value: 1 },
+          { id: 2, name: 'name2', value: 2 },
+        ]"
+        item-title="name"
+      >
+        <template v-slot:selection="{ item }">
+          <div class="selected-view">
+            選択されたアイテム: {{ item.value }}: {{ item.title }}
+          </div>
+        </template>
+        <template v-slot:item="{ item }">
+          <div
+            class="custom-item-view"
+            @click="onClickCustomItem(item.value)"
+          >
+            カスタムアイテム: {{ item.title }}
+          </div>
+        </template>
+      </v-select>
+    </client-only>
   </div>
 </template>
 
 <style scoped lang="scss">
 .index-page {
   font-size: 16px;
+}
+
+.selected-view {
+  color: #ff0000;
+}
+
+.custom-item-view {
+  color: #339933;
 }
 </style>
