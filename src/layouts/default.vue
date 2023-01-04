7278798e4bbf5d8ab37fe6a4922f6a30ed376008
motomichi <motomichi.motomichi@gmail.com>
2023-01-04 12:50:19 +0900

    src/layouts/default.vueとsrc/pages/index.vueを作成してsrc/app.vueに適用

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="default-layout">
+    <div>default-layout</div>
+    <ul>
+      <li
+        v-for="(item, index) in [
+          { name: 'home', path: '/'},
+        ]"
+        :key="index"
+      >
+        <NuxtLink :to="item.path">{{ item.name }}</NuxtLink>
+      </li>
+    </ul>
+    <slot />
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<script setup lang="ts">
+const pageName = 'index-page';
+</script>
+
+<template>
+  <div class="index-page">
+    <div>{{ pageName }}</div>
+  </div>
+</template>
+
+<style scoped>
+.index-page {
+  font-size: 16px;
+}
+</style>
